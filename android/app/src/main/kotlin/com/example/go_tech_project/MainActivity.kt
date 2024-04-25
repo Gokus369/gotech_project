b757882c0b27ace35b668da3305c58072efc6613
Gokus369 <samzzgokul@gmail.com>
2024-04-25 17:26:26 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/go_tech_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.go_tech_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/widgets/skeleton.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Skeleton extends StatelessWidget {
+  const Skeleton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
